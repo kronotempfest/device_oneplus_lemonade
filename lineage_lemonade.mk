ed716a0c45153f48ffa3b6f381b06d8985c7cd2f
kronotempfest <kronotempfest@gmail.com>
2023-02-22 09:39:18 +0000

    lemonade: #gtrcd
    Signed-off-by: kronotempfest <kronotempfest@gmail.com>

--- lineage_lemonade.mk
+++ lineage_lemonade.mk
@@ -8,12 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
+# Inherit from lemonade device.
 $(call inherit-product, device/oneplus/lemonade/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common RiceDroidOSS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device Identifier.
 PRODUCT_NAME := lineage_lemonade
 PRODUCT_DEVICE := lemonade
 PRODUCT_MANUFACTURER := OnePlus
@@ -31,3 +32,31 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:13/TP1A.220624.003/R.202208030717:user/release-keys
+
+# gtrcd
+RICE_CHIPSET := SDM888-5G
+RICE_MAINTAINER := jacktheripper
+
+# Targets
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_DISABLE_GRALLOC2_P010_SUPPORT := false
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_PRIVAPP_ENFORCEMENT := true
+TARGET_ENABLE_PIXEL_GBOARD_PADDINGS := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+TARGET_HAS_UDFPS := true
+TARGET_KERNEL_OPTIONAL_LD := false
+
+# Camera
+TARGET_USES_OPLUS_CAMERA := true
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+
+# Bootanimation
+SUSHI_BOOTANIMATION := 1080
+
+# GApps
+WITH_GMS := true
+TARGET_USE_GOOGLE_TELEPHONY := true
